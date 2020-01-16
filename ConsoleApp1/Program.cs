45c32c81265ed496e1c4a81e7dcfd117dde4a7c1
Neeru Gupta <neeru.gupta@wondermentapps.com>
2020-01-16 17:08:28 +0530

    Error

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Indresh Jenkins");
+            Console.WriteLine("Indresh Jenkins")
         }
     }
 }
